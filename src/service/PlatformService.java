a2b8d3775306769c1110fb860a65be5c7d501a53
孙锐 <sr19930829@163.com>
2016-11-21 08:20:31 +0800

    add acceptRequest service

--- src/service/PlatformService.java
+++ src/service/PlatformService.java
@@ -6,6 +6,7 @@ public interface PlatformService {
 	public Boolean removeExchange(String request_id, String user_from);
 	public Boolean declineExchange(String request_id, String user_to);
 	public Boolean removeOffer(String offer_id, String user_id);
+	public Boolean acceptRequest(Integer request_id);
 	public List<Request> showLatestTransaction(Integer sellerFrom, Integer sellerTo);
 	
 }
